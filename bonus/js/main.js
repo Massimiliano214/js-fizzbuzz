cb135b9f070ccc7b672c653cedacfa168485ed9a
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-02-24 15:44:58 +0100

    square colors added

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -1,4 +1,4 @@
-const containerDom = document.querySelector(".container")
+const containerDom = document.querySelector(".container");
 
 for (let i = 1; i <= 100; i++) {
 
@@ -7,24 +7,52 @@ for (let i = 1; i <= 100; i++) {
     
     if (( i % 3 == 0 ) && ( i % 5 == 0 )){
 
-        console.log("fizzbuzz")
+        console.log("fizzbuzz");
+        /*
         const square = `<div class="square">fizzbuzz</div>`;
         containerDom.innerHTML += square;
+        */
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.classList.add("fizzbuzz");
+        square.append("fizzbuzz");
+        containerDom.append(square);
 
     } else if ( i % 3 == 0 ) {
 
-        console.log("fizz")
+        console.log("fizz");
+        /*
         const square = `<div class="square">fizz</div>`;
         containerDom.innerHTML += square;
+        */
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.classList.add("fizz");
+        square.append("fizz");
+        containerDom.append(square);
 
     } else if ( i % 5 == 0 ) {
 
         console.log("buzz")
+        /*
         const square = `<div class="square">buzz</div>`;
         containerDom.innerHTML += square;
+        */
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.classList.add("buzz");
+        square.append("buzz");
+        containerDom.append(square);
     } else {
         console.log(i)
+
+        /*
         const square = `<div class="square">${i}</div>`;
         containerDom.innerHTML += square;
+        */
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.append(i);
+        containerDom.append(square);
     }
 }
